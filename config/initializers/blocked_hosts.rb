5427c1fd29eac862cb592b92f521be30f9d2dea2
sabmeua <sabme.ua@gmail.com>
2020-02-10 19:14:04 +0900

    Add comment

--- config/initializers/blocked_hosts.rb
+++ config/initializers/blocked_hosts.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
   config.hosts << "example.com"
   config.hosts << "baukis2.example.com"
+  # @note Set as "config.hosts << nil" when it doesn't need any restriction by host name.
 end
