d15e9afb39a8e5fb722ee004fda3870aa5234c9e
sabmeua <sabme.ua@gmail.com>
2020-02-10 18:40:12 +0900

    Update generator setting

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,14 @@ module Baukis2
     config.time_zone = "Tokyo"
     config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
     config.i18n.default_locale = :ja
+
+    config.generators do |g|
+      g.skip_routes true
+      g.helper false
+      g.assets false
+      g.test_framework :rspec
+      g.controller_specs false
+      g.view_specs false
+    end
   end
 end
