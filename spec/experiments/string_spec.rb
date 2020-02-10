4fef7c0d4038bd343a771e427532cc59dd338550
sabmeua <sabme.ua@gmail.com>
2020-02-10 19:54:22 +0900

    Add comment

--- spec/experiments/string_spec.rb
+++ spec/experiments/string_spec.rb
@@ -1,5 +1,8 @@
 require "spec_helper"
 
+# @note Test code is called "spec file". It is placed in "spec/" directory with name "*_spec.rb"
+#       Typically, spec files are placed sub directory such as, model class tests are placed "spec/models", API tests are placed "spec/requests", etc.
+
 describe String do
   describe "#<<" do
     example "文字の追加" do
