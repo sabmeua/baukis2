ce6d2d96f7728e76cbc51d2ca1aa7503d6adab36
sabmeua <sabme.ua@gmail.com>
2020-02-12 10:50:38 +0900

    Unhold failure example

--- spec/experiments/string_spec.rb
+++ spec/experiments/string_spec.rb
@@ -16,13 +16,12 @@ describe String do
     end
 
     #example "nilの追加" do
-    it "appends nil" do
-      pending('under investigation')
+    it "doesn't append nil" do
+      #pending('under investigation')
       # @note When it need to put the example on hold temporarily, use "pending" method to describe the reason.
       #       It can also do by replacing "example" with "xexample", "it" with "xit".
       s = "ABC"
-      s << nil
-      expect(s.size).to eq(4)
+      expect {s << nil}.to raise_error(TypeError)
     end
   end
 end
