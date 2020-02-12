e271bca77d75ba864d144029eb2da009bcfe2f26
sabmeua <sabme.ua@gmail.com>
2020-02-12 14:40:07 +0900

    Add helper method document_title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def document_title
+    # @note "present?" is a instance method of Object class defined by rails.
+    #       The entity of this method is negation of "blank?" method, also defined by rails.
+    if @title.present?
+      "#{@title} - Baukis2"
+    else
+      "Baukis2"
+    end
+  end
 end
