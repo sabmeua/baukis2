e89f6db8177044c372eb5a71d4ca12ad7b2d2925
sabmeua <sabme.ua@gmail.com>
2020-02-12 19:02:02 +0900

    Set layouts for each controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   layout :set_layout
 
   private def set_layout
-    if params[:controller].match(%r{\A(staff)/})
+    if params[:controller].match(%r{\A(staff|admin|customer)/})
       Regexp.last_match[1]
     else
       "application"
