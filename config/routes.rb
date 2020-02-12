75ca0e005968c0115a16cacec846eef62993caec
sabmeua <sabme.ua@gmail.com>
2020-02-12 13:31:03 +0900

    Add routing to the top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # @note The symbol specified by namespace method corresponds to a module.
+  #       namespace :staff { root 'top#index' } --> Staff::TopController#index
+
+  namespace :staff do
+    root "top#index"
+  end
+
+  namespace :admin do
+    root "top#index"
+  end
+
+  namespace :customer do
+    root "top#index"
+  end
 end
