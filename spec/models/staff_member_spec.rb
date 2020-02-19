23d7fd3526b0b5c615aba95442f22f672f0f3068
sabmeua <sabme.ua@gmail.com>
2020-02-19 14:04:05 +0900

    Add password= method to Staff model

--- spec/models/staff_member_spec.rb
+++ spec/models/staff_member_spec.rb
@@ -1,5 +1,18 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe StaffMember, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#password=" do
+    it "passes a string and hashed_password will be a 60 characters string" do
+      member = StaffMember.new
+      member.password = "baukis"
+      expect(member.hashed_password).to be_kind_of(String)
+      expect(member.hashed_password.size).to eq(60)
+    end
+
+    it "passes nil and hashed_password will be nil" do
+      member = StaffMember.new(hashed_password: "x")
+      member.password = nil
+      expect(member.hashed_password).to be_nil
+    end
+  end
 end
