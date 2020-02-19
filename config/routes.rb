55b60e44fca40c37b91979cba0f89e936f61f316
sabmeua <sabme.ua@gmail.com>
2020-02-19 18:01:21 +0900

    Update routing

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
 
   namespace :staff do
     root "top#index"
+    # @note Giving the "as" option allows to name the routing,
+    #       then it will be able to access as :staff_login in ERB template.
+    get "login" => "sessions#new", as: :login
+    post "session" => "sessions#create", as: :session
+    delete "session" => "sessions#destroy"
   end
 
   namespace :admin do
