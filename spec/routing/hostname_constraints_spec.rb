c3f0de30a2b568c8113f6ac2db93fd2f59426dd4
sabmeua <sabme.ua@gmail.com>
2020-02-26 16:06:37 +0900

    Add test case for routing

---
+++ spec/routing/hostname_constraints_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe "Routing" do
+  it "tests staff top page" do
+    config = Rails.application.config.baukis2
+    url = "http://#{config[:staff][:host]}/#{config[:staff][:path]}"
+    expect(get: url).to route_to(
+      host: config[:staff][:host],
+      controller: "staff/top",
+      action: "index"
+    )
+  end
+
+  it "tests admin login page" do
+    config = Rails.application.config.baukis2
+    url = "http://#{config[:admin][:host]}/#{config[:admin][:path]}/login"
+    expect(get: url).to route_to(
+      host: config[:admin][:host],
+      controller: "admin/sessions",
+      action: "new"
+    )
+  end
+
+  it "isn't routable if the host name isn't defined" do
+    expect(get: "http://foo.example.jp").not_to be_routable
+  end
+
+  it "isn't routable if the path isn't defined" do
+    config = Rails.application.config.baukis2
+    expect(get: "http://#{config[:staff][:host]}/xyz").not_to be_routable
+  end
+end
