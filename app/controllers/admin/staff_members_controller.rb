598d5deb16f1565959499908c8e735e1e6f5e9eb
sabmeua <sabme.ua@gmail.com>
2020-02-28 17:30:48 +0900

    Add admin authorization

--- app/controllers/admin/staff_members_controller.rb
+++ app/controllers/admin/staff_members_controller.rb
@@ -1,4 +1,6 @@
 class Admin::StaffMembersController < Admin::Base
+  before_action :authorize
+
   def index
     @staff_members = StaffMember.order(:family_name_kana, :given_name_kana)
   end
@@ -39,18 +41,25 @@ class Admin::StaffMembersController < Admin::Base
     end
   end
 
+  def destroy
+    staff_member = StaffMember.find(params[:id])
+    staff_member.destroy!
+    flash.notice = "職員アカウントを削除しました。"
+    redirect_to :admin_staff_members
+  end
+
+  private def authorize
+    unless current_administrator
+      flash.alert = "管理者としてログインしてください。"
+      redirect_to :admin_login
+    end
+  end
+
   private def staff_member_params
     params.require(:staff_member).permit(
       :email, :password, :family_name, :given_name,
       :family_name_kana, :given_name_kana,
       :start_date, :end_date, :suspended
     )
-  end
-
-  def destroy
-    staff_member = StaffMember.find(params[:id])
-    staff_member.destroy!
-    flash.notice = "職員アカウントを削除しました。"
-    redirect_to :admin_staff_members
   end
 end
