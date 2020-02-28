ac49f24774feb1030323049e4d117de03b2f1858
sabmeua <sabme.ua@gmail.com>
2020-02-28 17:34:48 +0900

    Refactor admin authorization

--- app/controllers/admin/base.rb
+++ app/controllers/admin/base.rb
@@ -1,7 +1,16 @@
 class Admin::Base < ApplicationController
+  before_action :authorize
+
   private def current_administrator
     if session[:administrator_id]
       @current_administrator ||= Administrator.find_by(id: session[:administrator_id])
+    end
+  end
+
+  private def authorize
+    unless current_administrator
+      flash.alert = "管理者としてログインしてください。"
+      redirect_to :admin_login
     end
   end
 
